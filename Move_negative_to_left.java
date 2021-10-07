ab5dd250d55f522871d6e771466fe7909b11a803
Prince Tomar <71916212+princetomar@users.noreply.github.com>
2021-10-07 22:58:33 +0530

    Add files via upload

---
+++ Move_negative_to_left.java
@@ -0,0 +1,38 @@
+package GFG_Arrays;
+
+public class Move_negative_to_left {
+
+    public static void rearrange(int[] arr,int n)
+    {
+        int j=0,temp;
+        for(int i=0 ; i<n ; i++)
+        {
+            if (arr[i] < 0 )
+            {
+                if(i != j)
+                {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+                j++;
+            }
+        }
+    }
+
+    public static void main (String[] args) {
+        /* code */
+
+
+        int arr[] =  {-1, 2, -3, 4, 5, 6, -7, 8, 9};
+        int n = arr.length;
+
+
+        rearrange(arr,n);
+
+        for(int i=0 ; i< n ; i++)
+        {
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
